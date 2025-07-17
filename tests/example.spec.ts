6c7358f5f7594611440179fd838c07be277855d5
Daniela Comanici <danielacomanici@Danielas-MacBook-Pro.local>
2025-07-17 12:07:36 -0600

    trying ... 2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,6 +6,8 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
   console.log("Console log from test: " + process.env.PASSWORD);
+  console.log("Console log from test, Printing the $SMTH values: " + process.env.SMTH);
+
 });
 
 test('get started link', async ({ page }) => {
